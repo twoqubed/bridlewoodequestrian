17de33f200de58a24e96959faf80ff97e37274a4
Ryan Breidenbach <ryan.breidenbach+github@gmail.com>
2016-04-10 11:58:23 -0500

    Add 2016 show schedule and lots of clean up

--- Events_files/Events.js
+++ Events_files/Events.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20140318
+
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,189),url:'Events_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Events_files/stroke_1.png'},{rect:new IWRect(1,-1,255,2),url:'Events_files/stroke_2.png'},{rect:new IWRect(256,-1,2,2),url:'Events_files/stroke_3.png'},{rect:new IWRect(256,1,2,189),url:'Events_files/stroke_4.png'},{rect:new IWRect(256,190,2,2),url:'Events_files/stroke_5.png'},{rect:new IWRect(1,190,255,2),url:'Events_files/stroke_6.png'},{rect:new IWRect(-1,190,2,2),url:'Events_files/stroke_7.png'}],new IWSize(257,191)),reflection_0:new IWReflection({opacity:0.00,offset:2.00})});registry.applyEffects();}
--- Horses_files/Horses.js
+++ Horses_files/Horses.js
@@ -1,5 +1,3 @@
-// Created by iWeb 3.0.4 local-build-20140923
-
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,190),url:'Horses_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Horses_files/stroke_1.png'},{rect:new IWRect(1,-1,255,2),url:'Horses_files/stroke_2.png'},{rect:new IWRect(256,-1,2,2),url:'Horses_files/stroke_3.png'},{rect:new IWRect(256,1,2,190),url:'Horses_files/stroke_4.png'},{rect:new IWRect(256,191,2,2),url:'Horses_files/stroke_5.png'},{rect:new IWRect(1,191,255,2),url:'Horses_files/stroke_6.png'},{rect:new IWRect(-1,191,2,2),url:'Horses_files/stroke_7.png'}],new IWSize(257,192)),reflection_0:new IWReflection({opacity:0.00,offset:2.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
